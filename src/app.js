d5b62db911fa0fef4f90e0bd5103f687f8258e92
Lucas <lucasmatiasblanco@outlook.com>
2023-07-06 10:24:58 -0300

    Move app.js to src folder

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const createError = require('http-errors');
+const express = require('express');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
