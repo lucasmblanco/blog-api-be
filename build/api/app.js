c551f0abff9e04fbbe2ae5eee6df2ccb5a022d5d
Lucas <lucasmatiasblanco@outlook.com>
2023-08-09 21:29:42 -0300

    Added credential:true in cors

--- build/api/app.js
+++ build/api/app.js
@@ -18,7 +18,9 @@ var app = (0, _express["default"])();
 (0, _databaseConfiguration.main)()["catch"](function (err) {
   return console.log(err);
 });
-app.use((0, _cors["default"])());
+app.use((0, _cors["default"])({
+  credentials: true
+}));
 app.use((0, _morgan["default"])('dev'));
 app.use(_express["default"].json());
 app.use(_passport["default"].initialize());
